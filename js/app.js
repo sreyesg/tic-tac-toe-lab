5b15c39499b7d700e0bf0c4bf1726fed43caeb9a
sreyesg <santiagoreyes.g@gmail.com>
2024-07-26 20:21:24 -0700

    add initialize function to render initial state of game

--- js/app.js
+++ js/app.js
@@ -1,18 +1,41 @@
 /*-------------------------------- Constants --------------------------------*/
 
-
-
 /*---------------------------- Variables (state) ----------------------------*/
 
+let board
+let turn
+let winner
+let tie
 
 /*------------------------ Cached Element References ------------------------*/
+
+const squareEls = document.querySelectorAll('.sqr')
+const messageEl = document.querySelector('#message')
 
 
 
 /*-------------------------------- Functions --------------------------------*/
+const init = () => {
+    console.log('init function worked')
+    board = ['','','','','','','','','','']
+    turn = 'x'
+    winner = false
+    tie = false
+    render ()
+}
+
+const render = () => {
+
+}
+const updateBoard = () => {
+    board.map((element, idx) => {
+        console.log(squareEls[idx]) 
+    })
+}
 
 
-
+init()
+updateBoard()
 /*----------------------------- Event Listeners -----------------------------*/
 
 
